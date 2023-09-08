6ede617dd98f1a2481a3e950daec8c4acbfe671f
i0p013q <ilampirai.pandian@gmail.com>
2023-09-08 11:50:52 +0530

    First Commit

--- HelloJava.java
+++ HelloJava.java
@@ -4,6 +4,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		System.out.println("HEllo Java Git Jenkins");
+                System.out.println(20+30);
 		// TODO Auto-generated method stub
 
 	}
